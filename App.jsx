345dbb3ba13b3f4a9d9b3d46555350a1a40be710
adityarajput751 <adityarajput751@gmail.com>
2023-05-11 20:01:07 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      
+    </SafeAreaView>
+  )
+}
+
+export default App
